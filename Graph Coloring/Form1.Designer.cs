cda0ec2c2889d2268eb6740b4e45c4b93bb67310
Thiago <thiago.kaique01@gmail.com>
2021-03-07 13:54:49 -0300

    [fix] events

--- Graph Coloring/Form1.Designer.cs
+++ Graph Coloring/Form1.Designer.cs
@@ -39,6 +39,9 @@ namespace Graph_Coloring
             this.ClientSize = new System.Drawing.Size(757, 506);
             this.Name = "Form1";
             this.Text = "Graph Coloring";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
             this.ResumeLayout(false);
 
         }
